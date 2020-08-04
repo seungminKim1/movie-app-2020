7547ffb152b1cef7723a7748d2e62bdd53dc3b61
SeungminKim <ksm0057@naver.com>
2020-08-04 22:11:03 +0900

    add Ex

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Ex from "./Ex";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Ex />
     </div>
   );
 }
---
+++ src/Ex.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Ex() {
+  return <h3>Example</h3>;
+}
+
+export default Ex;
