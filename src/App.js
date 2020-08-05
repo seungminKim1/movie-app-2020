9457d434817a4c0cf4672fc512a70bd967c19d69
SeungminKim <ksm0057@naver.com>
2020-08-05 23:20:28 +0900

    rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from "react";
+
 //{fav } = props.fav
-function Food({ fav }) {
-  return <h1>I like {fav} </h1>;
+function Food({ name, from }) {
+  return (
+    <div>
+      <h2>I like {name} </h2>
+      <h3>from {from}</h3>
+    </div>
+  );
 }
+
+const foodILike = [
+  {
+    name: "sushi",
+    from: "Japan",
+  },
+  {
+    name: "ramen",
+    from: "Korea",
+  },
+  {
+    name: "taco",
+    from: "mexico",
+  },
+  {
+    name: "water",
+    from: "nature",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <Food fav="sushi" />
-      <Food fav="pizza" />
-      <Food fav="burger" />
-      <Food fav="water" />
+      {foodILike.map((dish) => (
+        <Food name={dish.name} from={dish.from} />
+      ))}
     </div>
   );
 }
