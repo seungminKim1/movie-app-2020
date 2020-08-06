879d683113dbb43bc68145f5d3e3b21e03bd48a9
SeungminKim <ksm0057@naver.com>
2020-08-06 22:18:47 +0900

    install and add propTypes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,64 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //{fav } = props.fav
-
-function Food({ name, from }) {
-  return (
-    <div>
-      <h2>I like {name} </h2>
-      <h3>from {from}</h3>
-    </div>
-  );
-}
 
 const foodILike = [
   {
     id: 1,
     name: "sushi",
     from: "Japan",
+    rating: 5,
   },
   {
     id: 2,
     name: "ramen",
     from: "Korea",
+    rating: 5,
   },
   {
     id: 3,
     name: "taco",
     from: "mexico",
+    rating: 4,
   },
   {
     id: 4,
     name: "water",
     from: "nature",
+    rating: 5,
   },
 ];
 
-function renderFood(dish) {
-  return <Food id={dish.id} name={dish.name} from={dish.from} />;
+function Food({ name, from, rating }) {
+  return (
+    <div>
+      <h2>I like {name} </h2>
+      <h3>from {from}</h3>
+      <h4>{rating}/5.0</h4>
+    </div>
+  );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  from: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
 function App() {
-  return <div className="App">{foodILike.map(renderFood)}</div>;
+  return (
+    <div>
+      {foodILike.map((dish) => (
+        <Food
+          key={dish.id}
+          name={dish.name}
+          from={dish.from}
+          rating={dish.rating}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
