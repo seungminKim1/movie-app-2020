6779dfacdfc681c3c7b5fd6c63051caeada79dbf
SeungminKim <ksm0057@naver.com>
2020-08-08 19:39:03 +0900

    state2

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends React.Component {
   };
 
   add = () => {
-    console.log("add");
+    this.setState((current) => ({ count: current.count + 1 }));
   };
 
   remove = () => {
-    console.log("minus");
+    this.setState((current) => ({ count: current.count - 1 }));
   };
 
   render() {
