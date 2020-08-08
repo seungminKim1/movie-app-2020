17717429998be8d9211459a1719e4443dca206f3
SeungminKim <ksm0057@naver.com>
2020-08-08 20:08:35 +0900

    before fetching

--- src/App.js
+++ src/App.js
@@ -5,28 +5,19 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  remove = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-  componentDidUpdate() {
-    console.log("Updated");
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.remove}>Remove</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 //this.add 는 함수 자체를 onClick 프로퍼티에 할당해두는것, this.add() 는 add 함수를 '실행'한 결과를 담는것
+//react life cycle
 export default App;
