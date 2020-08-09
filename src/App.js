599a2a048ecc9d0fba80167b3b9aaa84f6774e01
SeungminKim <ksm0057@naver.com>
2020-08-09 13:49:11 +0900

    how to async

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 //{fav } = props.fav
 
@@ -8,10 +8,11 @@ class App extends React.Component {
     isLoading: true,
     movies: [],
   };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+  };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
     const { isLoading } = this.state;
