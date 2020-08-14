887d9bc418d64e3f738e1488c0ffb7e6c063a7d1
SeungminKim <ksm0057@naver.com>
2020-08-14 19:01:51 +0900

    add Detail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
 import "./App.css";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" component={About}></Route>
+      <Route path="/movie-detail" component={Detail}></Route>
     </HashRouter>
   );
 }
